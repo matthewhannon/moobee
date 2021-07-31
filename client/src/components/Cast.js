3a3f4b9ca6c2e2368e6459be2d92090ca3cc2faa
Matt Hannon <matthannon88@gmail.com>
2021-07-31 13:58:39 -0700

    ccast page will not show if film cast isn't loaded

--- client/src/components/Cast.js
+++ client/src/components/Cast.js
@@ -25,31 +25,39 @@ const Cast = () => {
     const showCast = () => {
         let { cast } = filmCast.data;
 
-        const castList = cast.map(({name, character, id, profile_path }, i) => {
-            return (
-                <Link className="cast-card-a-container" to={`/actor/${id}`} key={i}>
-                    <div className="cast-card">
-                        <div className="cast-card-info">
-                            <p className="cast-card-name" >{name}</p>
-                            {separateCharacters(character, 'cast-card-character')}
+        if(filmCast.loaded && cast.length > 0) {
+            const castList = cast.map(({name, character, id, profile_path }, i) => {
+                return (
+                    <Link className="cast-card-a-container" to={`/actor/${id}`} key={i}>
+                        <div className="cast-card">
+                            <div className="cast-card-info">
+                                <p className="cast-card-name" >{name}</p>
+                                {separateCharacters(character, 'cast-card-character')}
+                            </div>
+                            {profile_path ? <img className="cast-card-image" src={`https://image.tmdb.org/t/p/w185/${profile_path}`} alt={`${name}`}></img> : null}
                         </div>
-                        {profile_path ? <img className="cast-card-image" src={`https://image.tmdb.org/t/p/w185/${profile_path}`} alt={`${name}`}></img> : null}
+                    </Link>
+                )
+            })
+            return (
+                <div className="cast-page">
+                    <h2 className="cast-page-title">Cast</h2>
+                    <div className="cast-list">
+                        <ShowResults>
+                            {castList}
+                        </ShowResults>
                     </div>
-                </Link>
+                </div>
             )
-        })
-        return castList
+        } else {
+            return null;
+        }
     }
 
     return (
-        <div className="cast-page">
-            <h2 className="cast-page-title">Cast</h2>
-            <div className="cast-list">
-                <ShowResults>
-                    {filmCast.loaded && showCast()}
-                </ShowResults>
-            </div>
-        </div>
+        <React.Fragment>
+            {filmCast.loaded && showCast()}
+        </React.Fragment>
     );
 }
 
